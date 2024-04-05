93e9ef293478f27ee79bccafc23bf9662e21479f
Hulala <yang.ye.1@hotmail.com>
2024-04-06 00:02:21 +0200

    Primera version proyecto

--- index.js
+++ index.js
@@ -2,71 +2,73 @@ const fs = require("fs");
 const pdfParse = require("pdf-parse");
 const ExcelJS = require("exceljs");
 
-const Parameter = {
-  NumeroFactura: { Selector: "Nº factura", Separator: ":", Value: "" },
-  NombreCliente: { Selector: "Titular del contrato", Separator: ":", Value: "" },
-  NIF: { Selector: "NIF", Separator: ":", Value: "" },
-  CUPS: { Selector: "CUPS", Separator: ":", Value: "" },
-  TarifaAcceso: { Selector: "Peaje de transporte y distribución", Separator: ":", Value: "" },
-  Fecha: { Selector: "Periodo de facturación", Separator: ":", Value: "" },
-  FechaFactura: { Selector: "Fecha emisión factura", Separator: ":", Value: "" },
-  ImporteTotalFactura: { Selector: "Total", Separator: "Total", Value: "", Check: "€" },
-  ImportePotencia: { Selector: "Potencia", Separator: "Potencia", Value: "", Check: "€" },
-  ImporteEnergia: { Selector: "Energía", Separator: "Energía", Value: "", Check: "€" },
-  ImporteOtros: { Selector: "Otros", Separator: "Otros", Value: "", Check: "€" },
-  ImporteImpuesto: { Selector: "Impuestos", Separator: "Impuestos", Value: "", Check: "€" },
-  ConsumoPunta: { Selector: "Punta", Separator: "Punta", Value: "", Check: "." },
-  ConsumoLlano: { Selector: "Llano", Separator: "Llano", Value: "", Check: "." },
-  ConsumoValle: { Selector: "Valle", Separator: "Valle", Value: "", Check: "." },
-};
-const extractData = (lines) => {
+const ParameterPath = "./archivos/PdfToExcel_Parametros.xlsx";
+const ExcelOutputPath = "./archivos/ExcelResumenFactura.xlsx";
+const FolderPath = "./archivos/";
+
+main();
+async function main() {
+  const workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile(ParameterPath);
+  const parameterrow = ["archivo"];
+  workbook
+    .getWorksheet(1)
+    .getColumn(1)
+    .eachCell((value) => parameterrow.push(...[value.value.toString()]));
+  worksheetResumen.addRow(parameterrow);
+  for (const w of workbook.worksheets) {
+    const file = fs.readdirSync(FolderPath + w.name);
+    for (const f of file) {
+      const pdfData = await extractTextFromPDF(FolderPath + w.name + "/" + f);
+      if (w.name === "nexus") console.log(pdfData);
+      const lines = pdfData.text.split("\n");
+      const extractedData = extractData(lines, w.getSheetValues());
+      await writeDataToExcel(extractedData, ExcelOutputPath, w.name, f);
+    }
+  }
+}
+
+const extractData = (lines, parameter) => {
   const data = {};
-  lines.forEach((line) => {
-    for (const item of Object.keys(Parameter)) {
-      if (line.includes(Parameter[item].Selector)) {
-        if (Parameter[item].Check) {
-          if (line.split(Parameter[item].Separator)[1]?.trim().includes(Parameter[item].Check)) {
-            data[item] = line.split(Parameter[item].Separator)[1]?.trim();
+  for (let i = 1; i < parameter.length - 1; i++) data[parameter[i + 1][1]] = "";
+  lines.forEach((line, index) => {
+    for (let i = 1; i < parameter.length - 1; i++) {
+      if (parameter[i + 1][2]) {
+        if (line.includes(parameter[i + 1][2])) {
+          data[parameter[i + 1][1]] = line.split(parameter[i + 1][2])[1]?.trim();
+          if (parameter[i + 1][8]) {
+            data[parameter[i + 1][1]] = lines[index + parameter[i + 1][8]]?.trim();
           }
-        } else {
-          data[item] = line.split(Parameter[item].Separator)[1]?.trim();
+          if (parameter[i + 1][3] && !data[parameter[i + 1][1]].includes(parameter[i + 1][3])) {
+            data[parameter[i + 1][1]] = "";
+          }
+          if (parameter[i + 1][4]) {
+            data[parameter[i + 1][1]] = data[parameter[i + 1][1]].split(parameter[i + 1][4])[parameter[i + 1][5]]?.trim();
+            if (parameter[i + 1][6]) {
+              data[parameter[i + 1][1]] = data[parameter[i + 1][1]].split(parameter[i + 1][6])[parameter[i + 1][7]]?.trim();
+            }
+          }
+          if (parameter[i + 1][9]) {
+            data[parameter[i + 1][1]] = data[parameter[i + 1][1]].substring(parameter[i + 1][9]);
+          }
         }
-        console.log(item, ":", data[item]);
       }
     }
   });
   return data;
 };
+
 async function extractTextFromPDF(pdfPath) {
   const dataBuffer = fs.readFileSync(pdfPath);
   return await pdfParse(dataBuffer);
 }
-async function writeDataToExcel(data, excelPath) {
-  const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet("Factura Endesa");
-  worksheet.addRow(["Campo", "Valor"]);
-  Object.keys(data).forEach((key) => {
-    if (typeof data[key] === "object") {
-      Object.keys(data[key]).forEach((subKey) => {
-        worksheet.addRow([`${key} ${subKey}`, data[key][subKey]]);
-      });
-    } else {
-      worksheet.addRow([key, data[key]]);
-    }
-  });
 
-  await workbook.xlsx.writeFile(excelPath);
+const workbookResumen = new ExcelJS.Workbook();
+const worksheetResumen = workbookResumen.addWorksheet("Resumen");
+
+async function writeDataToExcel(data, excelPath, name, f) {
+  row = [name, f];
+  Object.keys(data).forEach((key) => row.push(...[data[key]]));
+  worksheetResumen.addRow(row);
+  await workbookResumen.xlsx.writeFile(excelPath);
 }
-
-async function pdfToExcel(pdfPath, excelPath) {
-  const pdfData = await extractTextFromPDF(pdfPath);
-  const lines = pdfData.text.split("\n");
-  const extractedData = extractData(lines);
-  await writeDataToExcel(extractedData, excelPath);
-  console.log(`Archivo Excel creado: ${excelPath}`);
-}
-
-const pdfPath = "./archivos/endesa.pdf";
-const excelPath = "./archivos/facturaEndesa.xlsx";
-
-pdfToExcel(pdfPath, excelPath).catch(console.error);
